77ded99158b370713c4d9dcfe6abb5ce1516cc29
Daichi Ishizaki <d1992820@yahoo.co.jp>
2023-03-23 16:37:49 +0900

    restore changes in Package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "HaishinKit",
     platforms: [
-        .iOS("13.0"),
+        .iOS(.v11),
         .tvOS(.v11),
         .macOS(.v10_13)
     ],
